a65054f49047cf087b5c26d3081f48a98f230c55
KaemonNg <kaemonng@KaemonNgs-MacBook-Air.local>
2026-04-20 10:05:56 +0800

    Flash Messaging and Interactivity with AlpineJS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-//
+import axios from 'axios';
+import Alpine from 'alpinejs';
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios = axios;
+window.Alpine = Alpine;
+Alpine.start();
--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -5,12 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? 'Idea' }}</title>
-    @vite(['resources/css/app.css'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-background text-foreground">
     <x-layout.nav/>
     <main>
         {{ $slot }}
     </main>
+
+    @session('success')
+        <div class="bg-primary px-4 py-3 absolute bottom-4 right-4 rounded-lg" x-data="{ show:true }" x-init="setTimeout(() => show = false, 3000)" x-show="show" x-transition.opacity.duration.300ms>{{ $value }}</div>
+    @endsession
+
 </body>
 </html>
